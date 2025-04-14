e6daae3f838f6a8d04582bb5e5db0077cb231458
Dayles1 <sololeveling0025@gmail.com>
2025-04-14 17:51:09 +0500

    +en/message.php

---
+++ lang/en/message.php
@@ -0,0 +1,16 @@
+<?php
+return [
+
+    'auth'=>[
+        'register'=>[
+            'success'=>'User registered successfully. Please check your email for verification link.',
+            'error'=>'Invalid credentials',
+        ],
+        'login'=>[
+            'success'=>'User logged in successfully.',
+            'error'=>'Invalid credentials',
+        ],
+        'logout'=>'Logout successfully.',
+
+    ]
+];
