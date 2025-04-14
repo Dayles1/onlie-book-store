a3e701e3f178834b1a27e505ddffbe7ea69c34ae
Dayles1 <sololeveling0025@gmail.com>
2025-04-14 18:21:47 +0500

    +route for login register and login

--- routes/v1/auth.php
+++ routes/v1/auth.php
@@ -1,2 +1,6 @@
 <?php
-Route:
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+        Route::post('register', [\App\Http\Controllers\AuthController::class, 'register']);
+        Route::post('login', [\App\Http\Controllers\AuthController::class, 'login']);
+        Route::post('logout', [\App\Http\Controllers\AuthController::class, 'logout'])->middleware('auth:sanctum');
