5f07348e3d69b9072d626ae70ff3e0422fce51f1
Dayles1 <sololeveling0025@gmail.com>
2025-04-14 17:54:01 +0500

    +auth messages in uzbek languages

---
+++ lang/uz/message.php
@@ -0,0 +1,15 @@
+<?php 
+  return [
+
+    'auth' => [
+        'register' => [
+            'success' => 'Foydalanuvchi muvaffaqiyatli roʻyxatdan oʻtdi. Iltimos, tasdiqlash havolasi uchun elektron pochtangizni tekshiring.',
+            'error' => 'Notoʻgʻri maʼlumotlar',
+        ],
+        'login' => [
+            'success' => 'Foydalanuvchi muvaffaqiyatli tizimga kirdi.',
+            'error' => 'Notoʻgʻri maʼlumotlar',
+        ],
+        'logout' => 'Tizimdan muvaffaqiyatli chiqildi.',
+    ]
+];
