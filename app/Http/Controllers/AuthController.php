4951c2bdf45e8b5daba17a3769d086533e6163bc
Dayles1 <sololeveling0025@gmail.com>
2025-04-14 17:27:02 +0500

    +register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'verification_token' => bin2hex(random_bytes(16)),
+            'password' => bcrypt($request->password),
+        ]);
+        
+    }
 }
