6ea06d0ddd7dcbf88429d8f32a58b7c6be67d5d2
Dayles1 <sololeveling0025@gmail.com>
2025-04-14 16:32:42 +0500

    +prapere translation

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,28 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    protected function prepareTranslations(array $translations, array $columns): array
+    {
+        $preparedTranslations = [];
+
+        foreach ($translations as $translation) {
+            foreach ($translation as $lang => $value) {
+                // Initialize the language key if not already present
+                if (!isset($preparedTranslations[$lang])) {
+                    $preparedTranslations[$lang] = [];
+                }
+
+                // Merge translation columns for the same language
+                foreach ($columns as $column) {
+                    if (isset($value[$column])) {
+                        $preparedTranslations[$lang][$column] = $value[$column];
+                    } else {
+                        info("{$column} not set for language: $lang");
+                    }
+                }
+            }
+        }
+
+        return $preparedTranslations;
+    }
 }
