c3bd716ee269768acf6d431b26bc36b31a8b81f7
Dayles1 <sololeveling0025@gmail.com>
2025-04-16 12:25:42 +0500

    Add Book model with fillable fields and relations for orders, categories, likes, and images

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -6,5 +6,30 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    //
+    protected $fillable = ['title', 'slug', 'description', 'author', 'price'];
+
+    protected $casts = [
+        'title' => 'array',
+        'description' => 'array',
+    ];
+
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
+    }
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class, 'category_books');
+    }
+
+    public function likes()
+    {
+        return $this->hasMany(Like::class);
+    }
+
+    // public function images()
+    // {
+    //     return $this->morphMany(Image::class, 'imageable');
+    // }
 }
