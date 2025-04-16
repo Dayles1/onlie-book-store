1fad64acfdb6d5c139ce2448e3d5339db7d4ca1a
Dayles1 <sololeveling0025@gmail.com>
2025-04-16 12:24:59 +0500

    Add Order model with fillable fields and relations for user and book

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -6,5 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    <?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    protected $fillable = ['book_id', 'user_id', 'address', 'stock', 'status'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function book()
+    {
+        return $this->belongsTo(Book::class);
+    }
+}
 }
