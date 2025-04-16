3136fa250bfbfe217238aee75d5e8405f7481af0
Dayles1 <sololeveling0025@gmail.com>
2025-04-16 12:09:26 +0500

    +colums to categories table

--- database/migrations/2025_04_16_070102_create_categories_table.php
+++ database/migrations/2025_04_16_070102_create_categories_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->string('slug')->unique();
+            $table->json('title'); 
+            $table->foreignId('parent_id')->nullable()->constrained('categories')->onDelete('cascade');
             $table->timestamps();
         });
     }
