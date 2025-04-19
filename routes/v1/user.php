62e3764cb082e2903ce9dd18d5b24a35e85e027c
Dayles1 <sololeveling0025@gmail.com>
2025-04-19 23:10:07 +0500

    Add routes for all UserController methods

---
+++ routes/v1/user.php
@@ -0,0 +1,8 @@
+<?php
+Route::middleware(['setLocale','auth:sanctum','checkStatus'])->group(function () {
+    Route::get('/user', [\App\Http\Controllers\UserController::class, 'index'])->name('user.index');
+    Route::get('/user/{id}', [\App\Http\Controllers\UserController::class, 'show'])->name('user.show');
+    Route::post('/user', [\App\Http\Controllers\UserController::class, 'store'])->name('user.store');
+    Route::put('/user/{id}', [\App\Http\Controllers\UserController::class, 'update'])->name('user.update');
+    Route::delete('/user/{id}', [\App\Http\Controllers\UserController::class, 'destroy'])->name('user.destroy');
+});
