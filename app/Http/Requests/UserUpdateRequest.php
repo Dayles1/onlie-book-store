176e2a5d6f321cef457c5c5ff6eb8494854b05eb
Dayles1 <sololeveling0025@gmail.com>
2025-04-19 23:02:39 +0500

    Add validation rules to UserUpdateRequest

--- app/Http/Requests/UserUpdateRequest.php
+++ app/Http/Requests/UserUpdateRequest.php
@@ -11,7 +11,7 @@ class UserUpdateRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,9 @@ class UserUpdateRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => 'nullable|string|max:255',    
+            'email' => 'nullable|email|exists:users,email,',
+            'password' => 'nullable|string|min:8',
         ];
     }
 }
