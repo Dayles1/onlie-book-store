89d78a0c136227a20b2deb9453a7bb13966e89be
Dayles1 <sololeveling0025@gmail.com>
2025-04-19 22:40:52 +0500

    +UserStoreRequest

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,5 +6,16 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        // Validate the request data
+       
+
+       $user = User::create([
+        'name'
+       ])
+
+        // Return a response
+        return response()->json(['message' => 'User created successfully', 'user' => $user], 201);
+    }
 }
