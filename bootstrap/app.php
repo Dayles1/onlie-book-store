56238a24169f5a526e75ed992f66bf2a17b508a3
Dayles1 <sololeveling0025@gmail.com>
2025-04-19 21:53:26 +0500

    +name for CheckStatusMiddleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
             'setLocale' => \App\Http\Middleware\SetLocaleMiddleware::class,
+            'checkStatus' => \App\Http\Middleware\CheckStatusMiddleware::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
