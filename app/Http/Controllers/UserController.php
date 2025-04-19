f2ec3b78cf3d5e10ca6b88cadb2ce18c23ac9ec9
Dayles1 <sololeveling0025@gmail.com>
2025-04-19 22:47:27 +0500

    +fake user create method

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,20 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Requests\UserStoreRequest;
 
 class UserController extends Controller
 {
-    public function store(Request $request)
+    public function store(UserStoreRequest $request)
     {
         // Validate the request data
        
 
        $user = User::create([
-        'name'
-       ])
+        'name'=> $request->name,
+        'email'=> $request->email,
+        'password'=> bcrypt($request->password),
+        'email_verified_at'=> now(),
+       ]);
 
-        // Return a response
-        return response()->json(['message' => 'User created successfully', 'user' => $user], 201);
+        return response()->json(['message' => __('message.user.create_success'), 'user' => $user], 201);
     }
 }
