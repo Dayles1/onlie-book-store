d4f3e4b8f542996d3d945b0922950c56a4b91865
Dayles1 <sololeveling0025@gmail.com>
2025-04-20 20:10:44 +0500

    +fillable to PostTranslation

--- app/Models/PostTranslation.php
+++ app/Models/PostTranslation.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class PostTranslation extends Model
 {
-    //
+    public $timestamps = false;
+
+    protected $fillable = [
+        'title',
+        'description',
+    ];
+
 }
