a30dc0d9af2941a79e06cc1fb3b0e2d99d964e62
Dayles1 <sololeveling0025@gmail.com>
2025-04-20 22:53:59 +0500

    require book

--- routes/api.php
+++ routes/api.php
@@ -9,4 +9,5 @@ Route::prefix('v1')->group(function () {
     require __DIR__ . '/v1/auth.php';
     require __DIR__ . '/v1/user.php';
     require __DIR__ . '/v1/category.php';
+    require __DIR__ . '/v1/book.php';
 });
