e1d95495ee071364ac75dbfcae6ef9b1f9b909f8
Dayles1 <sololeveling0025@gmail.com>
2025-04-20 22:58:38 +0500

    +routes for Categorycontroller

---
+++ routes/v1/category.php
@@ -0,0 +1,9 @@
+<?php
+
+Route::middleware(['setLocale','auth:sanctum'])->group(function () {
+    Route::get('/categories', [\App\Http\Controllers\CategoryController::class, 'index'])->name('categories.index');
+    Route::get('/categories/{category}', [\App\Http\Controllers\CategoryController::class, 'show'])->name('categories.show');
+    Route::post('/categories', [\App\Http\Controllers\CategoryController::class, 'store'])->name('categories.store');
+    Route::put('/categories/{category}', [\App\Http\Controllers\CategoryController::class, 'update'])->name('categories.update');
+    Route::delete('/categories/{category}', [\App\Http\Controllers\CategoryController::class, 'destroy'])->name('categories.destroy');
+}); 
