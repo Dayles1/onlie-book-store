6f082c4911fe082300643eb9a3df1785b660e861
Dayles1 <sololeveling0025@gmail.com>
2025-04-20 13:31:43 +0500

    Resolved conflict in CheckStatusMiddleware.php

--- app/Http/Middleware/CheckStatusMiddleware.php
+++ app/Http/Middleware/CheckStatusMiddleware.php
@@ -4,10 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-<<<<<<< HEAD
 use Illuminate\Support\Facades\Auth;
-=======
->>>>>>> bb751307b77b17783c9fe9784738c97aab362d49
 use Symfony\Component\HttpFoundation\Response;
 
 class CheckStatusMiddleware
@@ -18,24 +15,16 @@ class CheckStatusMiddleware
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
     public function handle(Request $request, Closure $next): Response
-<<<<<<< HEAD
     {
-        $user=Auth::user();
-        if ($user->status == 'user') {
+        $user = Auth::user();
+
+        if ($user->status !== 'admin') {
             return response()->json([
                 'message' => __('messages.user.status'),
                 'status' => false,
             ], 403);
-            
-        }
-        
-=======
-    {   
-        if ( $user->status !== 'admin') {
-             return response()->json(['message' => 'Unauthorized.'], 403);
         }
 
->>>>>>> bb751307b77b17783c9fe9784738c97aab362d49
         return $next($request);
     }
 }
