09df3d46a01efe5d27ce57225c96f90c73f93786
Dayles1 <sololeveling0025@gmail.com>
2025-04-22 15:18:13 +0500

    rename CategoryTranslation

--- database/migrations/2025_04_16_070102_create_categories_table.php
+++ database/migrations/2025_04_16_070102_create_categories_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('slug')->unique()->nullable();
-            $table->string('title'); 
             $table->foreignId('parent_id')->nullable()->constrained('categories')->onDelete('cascade');
             $table->timestamps();
         });
