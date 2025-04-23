a7f5841f3e2bb3eb9896b005b3896f7df527150e
Dayles1 <sololeveling0025@gmail.com>
2025-04-23 21:33:20 +0500

    +require new files

--- routes/api.php
+++ routes/api.php
@@ -10,4 +10,8 @@ Route::prefix('v1')->group(function () {
     require __DIR__ . '/v1/user.php';
     require __DIR__ . '/v1/category.php';
     require __DIR__ . '/v1/book.php';
+    require __DIR__ . '/v1/language.php';
+    require __DIR__ . '/v1/translation.php';
+    require __DIR__ . '/v1/like.php';    
+
 });
