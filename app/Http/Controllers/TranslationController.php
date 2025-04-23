f4786d2330f852c6d96adbd919a0738aec0848e3
Dayles1 <sololeveling0025@gmail.com>
2025-04-23 21:27:51 +0500

    php artisan make:controller TranslationController + index

--- app/Http/Controllers/TranslationController.php
+++ app/Http/Controllers/TranslationController.php
@@ -6,5 +6,17 @@ use Illuminate\Http\Request;
 
 class TranslationController extends Controller
 {
-    //
+    public function index(Request $request)
+{
+    $lang = $request->header('lang'); // получаем из заголовка запроса
+
+    $query = Translation::where('is_active', true);
+
+    if ($lang) {
+        $query->where('lang_prefix', $lang);
+    }
+
+    return response()->json($query->get());
+}
+
 }
