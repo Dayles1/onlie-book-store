5b11d869edf902b9d43e5b4bd20e37f08e3bf77e
Dayles1 <sololeveling0025@gmail.com>
2025-04-23 14:20:12 +0500

    update: slug from English title

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -14,19 +14,15 @@ class CategoryController extends Controller
 {
     public function store(CategoryStoreRequest $request)
     {
-        // Подготовим переводы заранее
         $translations = $this->prepareTranslations($request->translations, ['title']);
     
-        // Получим английский заголовок
-        $englishTitle = $translations['en']['title'] ?? 'category';
+        $englishTitle = $translations['en']['title'] ;
     
-        // Создадим категорию с заранее сгенерированным slug
         $category = Category::create([
             'parent_id' => $request->input('parent_id'),
             'slug' => Str::slug($englishTitle),
         ]);
     
-        // Применим переводы
         $category->fill($translations)->save();
     
         return $this->success(new CategoryResource($category), __('message.category.create_success'));
@@ -41,15 +37,22 @@ class CategoryController extends Controller
         return $this->success(new CategoryResource($category), __('message.category.show_success'));
     }
     public function update(CategoryUpdateRequest $request, $id)
-    {
-        $category = Category::find($id);
-        if (!$category) {
-            return $this->error(__('message.category.not_found'), 404);
-        }
-        $translations = $this->prepareTranslations($request->translations, ['title']);
-        $category->fill($translations)->save();
-        return $this->success(new CategoryResource($category), __('message.category.update_success'));
+{
+    $category = Category::find($id);
+    if (!$category) {
+        return $this->error(__('message.category.not_found'), 404);
     }
+
+    $translations = $this->prepareTranslations($request->translations, ['title']);
+
+    $englishTitle = $translations['en']['title'] ?? $category->slug;
+    $category->slug = Str::slug($englishTitle);
+
+    $category->fill($translations)->save();
+
+    return $this->success(new CategoryResource($category), __('message.category.update_success'));
+}
+
     public function destroy($id)
     {
         $category = Category::find($id);
