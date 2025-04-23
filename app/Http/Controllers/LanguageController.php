d9b59d106e2a69752917d5543aadf5cb0fb68b05
Dayles1 <sololeveling0025@gmail.com>
2025-04-23 21:30:44 +0500

    +index to languagecintroller

--- app/Http/Controllers/LanguageController.php
+++ app/Http/Controllers/LanguageController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Language;
 use Illuminate\Http\Request;
 
 class LanguageController extends Controller
 {
-    //
+    public function index()
+{
+    $langs = Language::where('is_active', true)->get();
+    return response()->json($langs);
+}
+
 }
