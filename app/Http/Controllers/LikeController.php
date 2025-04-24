388baa146db2c65ae03b6b1820ae0fd7b980bad0
Dayles1 <sololeveling0025@gmail.com>
2025-04-24 11:41:48 +0500

    +like index

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -1,10 +1,19 @@
 <?php
+namespace App\Http\Controllers\Api\V1\User;
 
-namespace App\Http\Controllers;
-
+use App\Http\Controllers\Controller;
+use App\Models\Like;
+use App\Models\Book;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LikeController extends Controller
 {
-    //
+    public function index()
+    {
+        $likes = Like::with('book')->where('user_id', Auth::id())->paginate(10);
+        return response()->json($likes);
+    }
+
+   
 }
