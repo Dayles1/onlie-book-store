36228f15f2061c02bd767849b45041bcdc616d24
Dayles1 <sololeveling0025@gmail.com>
2025-04-24 14:28:58 +0500

    i use advanced response in lang  cntroller

--- app/Http/Controllers/LanguageController.php
+++ app/Http/Controllers/LanguageController.php
@@ -1,16 +1,44 @@
 <?php
+namespace App\Http\Controllers\Api\V1\Admin;
 
-namespace App\Http\Controllers;
-
+use App\Http\Controllers\Controller;
 use App\Models\Language;
 use Illuminate\Http\Request;
 
 class LanguageController extends Controller
 {
     public function index()
-{
-    $langs = Language::where('is_active', true)->get();
-    return response()->json($langs);
+    {
+        $languages = Language::where('is_active', true)->get();
+        return $this->success($languages, __('message.lang.show_success'));
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'prefix' => 'required|string|unique:languages,prefix',
+        ]);
+
+        $language = Language::create($request->only('name', 'prefix', 'is_active'));
+        return $this->success($language, __('message.lang.create_success'), 201);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $language = Language::findOrFail($id);
+        $language->update($request->all());
+
+        return $this->success($language, __('message.lang.update_success'));
+    }
+
+    public function destroy($id)
+    {
+        $language = Language::find($id);
+        if (!$language) return $this->error(__('message.lang.not_found'), 404);
+
+        $language->delete();
+        return $this->success([], __('message.lang.delete_success'));
+    }
 }
 
-}
