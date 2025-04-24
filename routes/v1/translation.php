b5d9f5ea617707173ac23e7cc6ff6ca5d7aa8a8d
Dayles1 <sololeveling0025@gmail.com>
2025-04-24 14:32:54 +0500

    +other routes for translation

--- routes/v1/translation.php
+++ routes/v1/translation.php
@@ -1,8 +1,12 @@
 <?php 
 
-use App\Http\Controllers\TranslationController;
+use App\Http\Controllers\Api\V1\Admin\TranslationController;
 Route::middleware(['setLocale'])->group(function () {
     Route::get('/translations', [TranslationController::class, 'index']);
+    Route::post('/translations', [TranslationController::class, 'store']);
+    Route::put('/translations/{id}', [TranslationController::class, 'update']);
+    Route::delete('/translations/{id}', [TranslationController::class, 'destroy']);
+    Route::get('/translations/{id}', [TranslationController::class, 'show']);
 
 });
 // 'auth:sanctum'
