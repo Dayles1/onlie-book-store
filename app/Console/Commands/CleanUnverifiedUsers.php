685ed9476349f4406a7296ac491d8f540ab11578
Dayles1 <sololeveling0025@gmail.com>
2025-04-24 16:15:35 +0500

    logic for command

--- app/Console/Commands/CleanUnverifiedUsers.php
+++ app/Console/Commands/CleanUnverifiedUsers.php
@@ -1,8 +1,8 @@
-<?php
-
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use App\Models\User;
+use Carbon\Carbon;
 
 class CleanUnverifiedUsers extends Command
 {
@@ -18,13 +18,32 @@ class CleanUnverifiedUsers extends Command
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Delete users whose email is not verified and older than 3 days';
 
     /**
      * Execute the console command.
      */
     public function handle()
     {
-        //
+        $thresholdDate = Carbon::now()->subDays(3);
+
+        $users = User::whereNull('email_verified_at')
+                    ->where('created_at', '<=', $thresholdDate)
+                    ->get();
+
+        $count = $users->count();
+
+        if ($count === 0) {
+            $this->info('No unverified users older than 3 days found.');
+            return 0;
+        }
+
+        foreach ($users as $user) {
+            $this->line("Deleting user: {$user->email}");
+            $user->delete();
+        }
+
+        $this->info("Deleted $count unverified users.");
+        return 0;
     }
 }
