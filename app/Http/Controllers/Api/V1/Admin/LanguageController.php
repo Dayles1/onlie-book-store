31fec7a3759da2d7fa5f97a52ba37d96e4a63b33
Dayles1 <sololeveling0025@gmail.com>
2025-04-25 20:51:25 +0500

    +cache Languages  controller

--- app/Http/Controllers/Api/V1/Admin/LanguageController.php
+++ app/Http/Controllers/Api/V1/Admin/LanguageController.php
@@ -4,12 +4,16 @@ namespace App\Http\Controllers\Api\V1\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Language;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class LanguageController extends Controller
 {
     public function index()
     {
-        $languages = Language::where('is_active', true)->get();
+        $languages = Cache::remember('active_languages', 3600, function () {
+            return Language::where('is_active', true)->get();
+        });
+
         return $this->success($languages, __('message.lang.show_success'));
     }
 
@@ -21,6 +25,9 @@ class LanguageController extends Controller
         ]);
 
         $language = Language::create($request->only('name', 'prefix', 'is_active'));
+
+        Cache::forget('active_languages');
+
         return $this->success($language, __('message.lang.create_success'), 201);
     }
 
@@ -29,16 +36,22 @@ class LanguageController extends Controller
         $language = Language::findOrFail($id);
         $language->update($request->all());
 
+        Cache::forget('active_languages');
+
         return $this->success($language, __('message.lang.update_success'));
     }
 
     public function destroy($id)
     {
         $language = Language::find($id);
-        if (!$language) return $this->error(__('message.lang.not_found'), 404);
+        if (!$language) {
+            return $this->error(__('message.lang.not_found'), 404);
+        }
 
         $language->delete();
+
+        Cache::forget('active_languages');
+
         return $this->success([], __('message.lang.delete_success'));
     }
 }
-
