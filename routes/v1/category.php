900f48dadca173c1f67d70a8dd5bd986f4b340f5
Dayles1 <sololeveling0025@gmail.com>
2025-04-25 10:07:33 +0500

    fix category routes

--- routes/v1/category.php
+++ routes/v1/category.php
@@ -1,9 +1,11 @@
 <?php
 
 Route::middleware(['setLocale','auth:sanctum'])->group(function () {
-    Route::get('/categories', [\App\Http\Controllers\CategoryController::class, 'index'])->name('categories.index');
-    Route::get('/categories/{category}', [\App\Http\Controllers\CategoryController::class, 'show'])->name('categories.show');
     Route::post('/categories', [\App\Http\Controllers\CategoryController::class, 'store'])->name('categories.store');
     Route::put('/categories/{category}', [\App\Http\Controllers\CategoryController::class, 'update'])->name('categories.update');
     Route::delete('/categories/{category}', [\App\Http\Controllers\CategoryController::class, 'destroy'])->name('categories.destroy');
 }); 
+Route::middleware(['setLocale'])->group(function () {
+    Route::get('/categories', [\App\Http\Controllers\CategoryController::class, 'index'])->name('categories.index');
+    Route::get('/categories/{slug}', [\App\Http\Controllers\CategoryController::class, 'show'])->name('categories.show');
+});
