29bf7654b42e216aa6024ef306a74a0737a35cdd
Dayles1 <sololeveling0025@gmail.com>
2025-04-25 09:26:23 +0500

    +colums to exchange table

--- database/migrations/2025_04_25_042408_create_exchange_rates_table.php
+++ database/migrations/2025_04_25_042408_create_exchange_rates_table.php
@@ -10,12 +10,16 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
-    {
-        Schema::create('exchange_rates', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+{
+    Schema::create('exchange_rates', function (Blueprint $table) {
+        $table->id();
+        $table->string('code'); 
+        $table->decimal('rate', 20, 4); 
+        $table->date('date'); 
+        $table->timestamps();
+    });
+}
+
 
     /**
      * Reverse the migrations.
