ef0c6b6dacab39ae771c655a9b021b0a1bccc878
Dayles1 <sololeveling0025@gmail.com>
2025-04-25 09:52:35 +0500

    uograde book resource

--- app/Http/Resources/BookResource.php
+++ app/Http/Resources/BookResource.php
@@ -1,31 +1,29 @@
 <?php
-
 namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
-use App\Http\Resources\CategoryResource;
 use Illuminate\Http\Resources\Json\JsonResource;
+use App\Models\ExchangeRate;
 
 class BookResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @return array<string, mixed>
-     */
     public function toArray(Request $request): array
     {
+        $usd = ExchangeRate::where('code', 'USD')->orderByDesc('date')->first()?->rate ?? 1;
+        $rub = ExchangeRate::where('code', 'RUB')->orderByDesc('date')->first()?->rate ?? 1;
+
         return [
             'id' => $this->id,
-            'title' => $this->title,
-            'description' => $this->description,
+            'title' => $this->title, // Translation frontda hal qilinadi
             'slug' => $this->slug,
+            'description' => $this->description,
             'author' => $this->author,
-            'price' => $this->price,
-            'images' => ImageResource::collection($this->whenLoaded('images')),
-            'categories' => CategoryResource::collection($this->whenLoaded('categories')),
-            // 'orders' => OrderResource::collection($this->whenLoaded('orders')),
-            // 'likes' => LikeResource::collection($this->whenLoaded('likes')),
+            'price_uzs' => $this->price,
+            'price_usd' => round($this->price / $usd, 2),
+            'price_rub' => round($this->price / $rub, 2),
+            'categories' => $this->categories->pluck('title'),
+            'images' => $this->images->pluck('url'), // Agar polymorphic image bor bo‘lsa
+            'created_at' => $this->created_at->toDateTimeString(),
         ];
     }
 }
