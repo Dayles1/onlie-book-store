0f87c327cc7dece8d404fed44cf088ed48dcaf5f
Dayles1 <sololeveling0025@gmail.com>
2025-04-25 14:54:38 +0500

    php artisan make:controller Admin/NotificationController

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use App\Models\Order;
 use Illuminate\Http\Request;
+use App\Notifications\NewOrderNotification;
 
 class OrderController extends Controller
 {
-    //
+    public function store(Request $request)
+{
+    $order = Order::create([
+        'book_id' => $request->book_id,
+        'user_id' => auth()->id(),
+        'address' => $request->address,
+        'stock' => $request->stock,
+    ]);
+
+    // Adminlarga notification yuborish
+    $admins = User::where('role', 'admin')->get();
+    foreach ($admins as $admin) {
+        $admin->notify(new NewOrderNotification($order));
+    }
+
+    return response()->json(['message' => 'Buyurtma yaratildi'], 201);
+}
 }
