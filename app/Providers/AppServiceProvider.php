e8475343643b365965c3678fc9eda4c7260cb990
Dayles1 <sololeveling0025@gmail.com>
2025-04-25 21:01:03 +0500

    book + observer

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,8 +4,12 @@ namespace App\Providers;
 
 use App\Models\Book;
 use App\Models\Category;
+use App\Models\Language;
+use App\Models\Translation;
 use App\Observers\BookObserver;
 use App\Observers\CategoryObserver;
+use App\Observers\LanguageObserver;
+use App\Observers\TranslationObserver;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,5 +29,7 @@ class AppServiceProvider extends ServiceProvider
     {
         Book::observe(BookObserver::class);
         Category::observe(CategoryObserver::class);
+        Translation::observe(TranslationObserver::class);
+        Language::observe(LanguageObserver::class);
     }
 }
