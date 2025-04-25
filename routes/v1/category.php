70c224ac0b28036bc42cbee788de96b7ac038d8b
Dayles1 <sololeveling0025@gmail.com>
2025-04-25 21:34:40 +0500

    change category to slug

--- routes/v1/category.php
+++ routes/v1/category.php
@@ -2,8 +2,8 @@
 
 Route::middleware(['setLocale','auth:sanctum'])->group(function () {
     Route::post('/categories', [\App\Http\Controllers\CategoryController::class, 'store'])->name('categories.store');
-    Route::put('/categories/{category}', [\App\Http\Controllers\CategoryController::class, 'update'])->name('categories.update');
-    Route::delete('/categories/{category}', [\App\Http\Controllers\CategoryController::class, 'destroy'])->name('categories.destroy');
+    Route::put('/categories/{slug}', [\App\Http\Controllers\CategoryController::class, 'update'])->name('categories.update');
+    Route::delete('/categories/{slug}', [\App\Http\Controllers\CategoryController::class, 'destroy'])->name('categories.destroy');
 }); 
 Route::middleware(['setLocale'])->group(function () {
     Route::get('/categories', [\App\Http\Controllers\CategoryController::class, 'index'])->name('categories.index');
