41ff0f95abe36e30963ed0d82182b8bbc7701568
Dayles1 <sololeveling0025@gmail.com>
2025-04-26 14:09:10 +0500

    +book for admin and user

--- routes/v1/Admin/book.php
+++ routes/v1/Admin/book.php
@@ -1,15 +1,10 @@
 <?php 
 
-use App\Http\Controllers\BookController;
-Route::middleware(['setLocale','auth:sanctum'])->group(function () {
+use App\Http\Controllers\Api\V1\Admin\BookController;
+Route::middleware(['setLocale','auth:sanctum','checkStatus'])->group(function () {
     Route::post('/books', [BookController::class, 'store'])->name('books.store');
     Route::put('/books/{slug}', [BookController::class, 'update'])->name('books.update');
     Route::delete('/books/{slug}', [BookController::class, 'destroy'])->name('books.destroy');
 });
-Route::middleware(['setLocale','auth:sanctum'])->group(function () {
-    Route::get('/books', [BookController::class, 'index'])->name('books.index');
-    Route::get('/books/{slug}', [BookController::class, 'show'])->name('books.show');
-    Route::get('books/search', [BookController::class, 'search']);
 
-});
 
