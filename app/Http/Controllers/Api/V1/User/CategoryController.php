3979683f8a784f3aa39dc57c46d397abef9bcd67
Dayles1 <sololeveling0025@gmail.com>
2025-04-26 14:51:42 +0500

    -store update delete

--- app/Http/Controllers/Api/V1/User/CategoryController.php
+++ app/Http/Controllers/Api/V1/User/CategoryController.php
@@ -43,21 +43,7 @@ class CategoryController extends Controller
         );
     }
 
-    public function store(CategoryStoreRequest $request)
-    {
-        $translations = $this->prepareTranslations($request->translations, ['title']);
-    
-        $englishTitle = $translations['en']['title'];
-    
-        $category = Category::create([
-            'parent_id' => $request->input('parent_id'),
-            'slug' => Str::slug($englishTitle),
-        ]);
-    
-        $category->fill($translations)->save();
-    
-        return $this->success(new CategoryResource($category), __('message.category.create_success'));
-    }
+   
     public function search(Request $request)
     {
         $query = \App\Models\Book::query();
@@ -86,30 +72,4 @@ class CategoryController extends Controller
         
     }
     
-    public function update(CategoryUpdateRequest $request, $slug)
-    {
-        $category = Category::where('slug', $slug)->first();
-        if (!$category) {
-            return $this->error(__('message.category.not_found'), 404);
-        }
-
-        $translations = $this->prepareTranslations($request->translations, ['title']);
-        $englishTitle = $translations['en']['title'] ?? $category->slug;
-        $category->slug = Str::slug($englishTitle);
-
-        $category->fill($translations)->save();
-
-        return $this->success(new CategoryResource($category), __('message.category.update_success'));
-    }
-
-    public function destroy($slug)
-    {
-        $category = Category::where('slug', $slug)->first();
-        if (!$category) {
-            return $this->error(__('message.category.not_found'), 404);
-        }
-
-        $category->delete();
-        return $this->success(null, __('message.category.delete_success'));
-    }
 }
