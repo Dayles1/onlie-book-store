23e74ea3057e04288d5ec60d66ba54faff3d1e3d
Dayles1 <sololeveling0025@gmail.com>
2025-04-26 22:26:16 +0500

    php artisan make:model Setting

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,9 @@ Route::prefix('v1')->group(function () {
     require __DIR__ . '/v1/Admin/translation.php'; 
     
     require __DIR__ . '/v1/Admin/user.php';  
+    require __DIR__ . '/v1/Admin/user.php';  
 
+    
 //     //User
 
     require __DIR__ . '/v1/User/auth.php';
