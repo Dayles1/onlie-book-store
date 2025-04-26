de39a7bd83be1aa49fec8560111b5bdfb40d4499
Dayles1 <sololeveling0025@gmail.com>
2025-04-26 12:00:22 +0500

    +admin Order

--- routes/v1/Admin/order.php
+++ routes/v1/Admin/order.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\OrderController;
+use App\Http\Controllers\Api\V1\Admin\OrderController;
 
 Route::middleware(['auth:sanctum', 'setLocale'])->group(function () {
     Route::get('/order', [OrderController::class, 'index'])->name('order.index');
