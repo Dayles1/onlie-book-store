d2ec569d693ba8b273117f7329f81fdd445eb5b3
Dayles1 <sololeveling0025@gmail.com>
2025-04-26 20:02:46 +0500

    +txt

--- app/Observers/LanguageObserver.php
+++ app/Observers/LanguageObserver.php
@@ -1,4 +1,5 @@
-<?php 
+<?php
+
 namespace App\Observers;
 
 use App\Models\Language;
@@ -6,15 +7,31 @@ use Illuminate\Support\Facades\Cache;
 
 class LanguageObserver
 {
+   
     public function created(Language $language)
     {
+        $this->updateCache();
     }
 
+   
     public function updated(Language $language)
     {
+        $this->updateCache();
     }
 
+    
     public function deleted(Language $language)
     {
+        $this->updateCache();
+    }
+
+    
+    
+    protected function updateCache()
+    {
+        Cache::forget('active_languages');
+        Cache::remember('active_languages', 3600, function () {
+            return Language::where('is_active', true)->get();
+        });
     }
 }
