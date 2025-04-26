f3974074d9d0aeaa7224fbc245b6b4893ad0dc45
Dayles1 <sololeveling0025@gmail.com>
2025-04-26 21:01:47 +0500

    OrderUpdateRequest + validate

--- app/Http/Requests/OrderUpdateRequest.php
+++ app/Http/Requests/OrderUpdateRequest.php
@@ -11,7 +11,7 @@ class OrderUpdateRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,8 @@ class OrderUpdateRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'address' => 'nullable|string|max:255',
+            'stock' => 'nullable|integer|min:1',
         ];
     }
 }
