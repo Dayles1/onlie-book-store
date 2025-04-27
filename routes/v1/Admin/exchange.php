cb84a8a05f8f4ffb1f0ae11c513aa26fafd40dbe
Dayles1 <sololeveling0025@gmail.com>
2025-04-27 11:59:55 +0500

    +routes

---
+++ routes/v1/Admin/exchange.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Http\Controllers\Api\V1\Admin\ExchangeRateController;
+
+Route::middleware(['setLocale', 'sanctum', 'checkStatus'])->group(function () {
+    Route::get('/exchange-rates', [ExchangeRateController::class, 'index']);
+    Route::post('/exchange-rates', [ExchangeRateController::class, 'store']);
+    Route::put('/exchange-rates/{id}', [ExchangeRateController::class, 'update']);
+    Route::delete('/exchange-rates/{id}', [ExchangeRateController::class, 'destroy']);
+});
