9889ca7cfb4f324ab097b847cbb92a1da6acec14
Dayles1 <sololeveling0025@gmail.com>
2025-04-27 12:25:29 +0500

    +auth sanctum

--- routes/v1/Admin/exchange.php
+++ routes/v1/Admin/exchange.php
@@ -2,7 +2,7 @@
 
 use App\Http\Controllers\Api\V1\Admin\ExchangeRateController;
 
-Route::middleware(['setLocale', 'sanctum', 'checkStatus'])->group(function () {
+Route::middleware(['setLocale', 'auth:sanctum', 'checkStatus'])->group(function () {
     Route::get('/exchange-rates', [ExchangeRateController::class, 'index']);
     Route::post('/exchange-rates', [ExchangeRateController::class, 'store']);
     Route::put('/exchange-rates/{id}', [ExchangeRateController::class, 'update']);
