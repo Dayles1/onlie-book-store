938855d75ecbe734389e0c981ed5c9783f68b2e8
Dayles1 <sololeveling0025@gmail.com>
2025-04-27 11:44:51 +0500

    +validate to update request

--- app/Http/Requests/ExchangeRateUpdateRequest.php
+++ app/Http/Requests/ExchangeRateUpdateRequest.php
@@ -11,7 +11,7 @@ class ExchangeRateUpdateRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,8 @@ class ExchangeRateUpdateRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'rate' => 'nullable|numeric',
+            'date' => 'nullable|date',
         ];
     }
 }
