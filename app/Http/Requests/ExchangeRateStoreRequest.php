9119964de66d0c2389263cfb2e6a38a71a5d6698
Dayles1 <sololeveling0025@gmail.com>
2025-04-27 11:43:24 +0500

    +validate to request

--- app/Http/Requests/ExchangeRateStoreRequest.php
+++ app/Http/Requests/ExchangeRateStoreRequest.php
@@ -11,7 +11,7 @@ class ExchangeRateStoreRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,9 @@ class ExchangeRateStoreRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'code' => 'required|string|max:3|unique:exchange_rates,code',
+            'rate' => 'required|numeric',
+            'date' => 'required|date',
         ];
     }
 }
