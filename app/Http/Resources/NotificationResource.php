2ca073fb50b04d2971a5ce7ddf1949272fb313f9
Dayles1 <sololeveling0025@gmail.com>
2025-05-01 13:04:40 +0500

    php artisan make:resource NotificationResource + logic

--- app/Http/Resources/NotificationResource.php
+++ app/Http/Resources/NotificationResource.php
@@ -14,6 +14,13 @@ class NotificationResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'type' => $this->type,
+            'data' => $this->data,
+            'read_at' => $this->read_at,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
     }
 }
