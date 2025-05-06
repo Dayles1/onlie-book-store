f802353762b082e6301199886e7f83ae6bd3dfdb
Dayles1 <Sololeveling0025@gmail.com>
2025-05-06 17:01:24 +0500

    + methods to AuthService

--- app/Interfaces/Interfaces/Services/AuthServiceInterface.php
+++ app/Interfaces/Interfaces/Services/AuthServiceInterface.php
@@ -4,5 +4,7 @@ namespace App\Interfaces\Interfaces\Services;
 
 interface AuthServiceInterface
 {
-    //
+    public function login(array $data);
+    public function register(array $data);
+    public function logout();
 }
