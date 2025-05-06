1d8e19fb9f462218061d057c90c21ac287556a4a
Dayles1 <Sololeveling0025@gmail.com>
2025-05-06 16:58:17 +0500

    +AuthService

---
+++ app/Services/AuthService.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Services;
+
+use App\Interfaces\Interfaces\Services\AuthServiceInterface;
+
+class AuthService   implements AuthServiceInterface
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
