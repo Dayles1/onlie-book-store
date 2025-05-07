c64f069a4d1eb8671c9ccad791168c58f972ff70
Dayles1 <Sololeveling0025@gmail.com>
2025-05-07 14:49:21 +0500

    +prapareTranslatio n to BaseService

--- app/Services/BaseService.php
+++ app/Services/BaseService.php
@@ -7,8 +7,29 @@ class BaseService
     /**
      * Create a new class instance.
      */
-    public function __construct()
+    protected function prepareTranslations(array $translations, array $columns): array
     {
-        //
+        $preparedTranslations = [];
+
+        foreach ($translations as $translation) {
+            foreach ($translation as $lang => $value) {
+                // Initialize the language key if not already present
+                if (!isset($preparedTranslations[$lang])) {
+                    $preparedTranslations[$lang] = [];
+                }
+
+                // Merge translation columns for the same language
+                foreach ($columns as $column) {
+                    if (isset($value[$column])) {
+                        $preparedTranslations[$lang][$column] = $value[$column];
+                    } else {
+                        info("{$column} not set for language: $lang");
+                    }
+                }
+            }
+        }
+
+        return $preparedTranslations;
     }
+    
 }
