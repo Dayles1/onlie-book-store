94c66f6045440afd1e94c7c47f54b25dfdc50540
Dayles1 <Sololeveling0025@gmail.com>
2025-05-07 12:47:32 +0500

    Bookservice imolemenst BookServiceInterface

--- app/Interfaces/Services/BookServiceIntarface.php
+++ app/Interfaces/Services/BookServiceIntarface.php
@@ -4,5 +4,10 @@ namespace App\Interfaces\Services;
 
 interface BookServiceIntarface
 {
-    //
+    public function index($request);
+    public function show($slug);
+    public function search($request);
+    public function store($data);
+    public function update($data);
+    public function destroy($book);
 }
--- app/Services/BookService.php
+++ app/Services/BookService.php
@@ -2,13 +2,37 @@
 
 namespace App\Services;
 
-class BookService
+use App\Interfaces\Services\BookServiceIntarface;
+
+class BookService implements BookServiceIntarface
 {
     /**
      * Create a new class instance.
      */
-    public function __construct()
-    {
-        //
-    }
+
+        public function index($request)
+        {
+            
+        }
+        public function show($slug)
+        {
+            // Logic for show
+        }
+        public function search($request)
+        {
+            // Logic for search
+        }
+        public function store($data)
+        {
+            // Logic for store
+        }
+        public function update($data)
+        {
+            // Logic for update
+        }
+        public function destroy($book)
+        {
+            // Logic for destroy
+        }
+    
 }
