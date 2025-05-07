fb4bf92a93956edbac964f0123d9f4de0d047776
Dayles1 <Sololeveling0025@gmail.com>
2025-05-07 13:23:12 +0500

    +implements methods

--- app/Interfaces/Services/CategoryServiceInterface.php
+++ app/Interfaces/Services/CategoryServiceInterface.php
@@ -4,5 +4,11 @@ namespace App\Interfaces\Services;
 
 interface CategoryServiceInterface
 {
-    
+ 
+    public function index();
+    public function show($slug);
+    public function search($request);
+    public function store($data);
+    public function update($data, $slug);
+    public function destroy($slug);
 }
