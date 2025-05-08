3ac42cf181a1f3956070aa89496cf03688e42622
Dayles1 <Sololeveling0025@gmail.com>
2025-05-08 20:55:59 +0500

    +userservie + userserviceinterface

--- app/Providers/ControllerServiceProvider.php
+++ app/Providers/ControllerServiceProvider.php
@@ -5,10 +5,12 @@ namespace App\Providers;
 use Auth;
 use App\Services\AuthService;
 use App\Services\BookService;
+use App\Services\UserService;
 use App\Services\CategoryService;
 use Illuminate\Support\ServiceProvider;
 use App\Interfaces\Services\AuthServiceInterface;
 use App\Interfaces\Services\BookServiceIntarface;
+use App\Interfaces\Services\UserServiceInterface;
 use App\Interfaces\Services\CategoryServiceInterface;
 
 class ControllerServiceProvider extends ServiceProvider
@@ -21,6 +23,7 @@ class ControllerServiceProvider extends ServiceProvider
         $this->app->bind(AuthServiceInterface::class, AuthService::class);
         $this->app->bind(BookServiceIntarface::class, BookService::class);
         $this->app->bind(CategoryServiceInterface::class, CategoryService::class);
+        $this->app->bind(UserServiceInterface::class, UserService::class);
     }
 
     /**
