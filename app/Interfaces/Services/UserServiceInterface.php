421414c0800168c2485603bb6867af6edeedf9de
Dayles1 <Sololeveling0025@gmail.com>
2025-05-08 19:14:18 +0500

    +UserServiceInterface methods

--- app/Interfaces/Services/UserServiceInterface.php
+++ app/Interfaces/Services/UserServiceInterface.php
@@ -4,5 +4,9 @@ namespace App\Interfaces\Services;
 
 interface UserServiceInterface
 {
-    
+    public function index($request);
+    public function store($request);
+    public function show($id);
+    public function update($request, $id);
+    public function destroy($id);
 }
