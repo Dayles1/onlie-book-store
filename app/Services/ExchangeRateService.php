ae657455806a6a5c80514fbd908c73830e98ce95
Dayles1 <Sololeveling0025@gmail.com>
2025-05-09 19:13:19 +0500

    +service methods fo exchangerate

--- app/Services/ExchangeRateService.php
+++ app/Services/ExchangeRateService.php
@@ -2,13 +2,21 @@
 
 namespace App\Services;
 
-class ExchangeRateService
+use App\Interfaces\Services\ExchangeRateServiceInterface;
+
+class ExchangeRateService extends BaseService implements ExchangeRateServiceInterface
 {
-    /**
-     * Create a new class instance.
-     */
-    public function __construct()
-    {
-        //
+    public function index(){
+
+    }
+    public function store($request){
+
+    }
+    public function update($data, $id){
+
+    }   
+    public function destroy($id){
+
     }
 }
+
