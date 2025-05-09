9e075a67cca1123a11ae71648df2e2a47349ea6d
Dayles1 <Sololeveling0025@gmail.com>
2025-05-09 19:08:28 +0500

    +inerface methods fo exchangerate

--- app/Interfaces/Services/ExchangeRateServiceInterface.php
+++ app/Interfaces/Services/ExchangeRateServiceInterface.php
@@ -4,5 +4,8 @@ namespace App\Interfaces\Services;
 
 interface ExchangeRateServiceInterface
 {
-    //
+    public function index(); 
+    public function store($request); 
+    public function update($data, $id);
+    public function destroy($id);    
 }
