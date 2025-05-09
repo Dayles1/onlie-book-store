badf3eef06305fd3159900840d99d66e1fcd9be1
Dayles1 <Sololeveling0025@gmail.com>
2025-05-09 23:33:24 +0500

    +destroy to LanguageServiceInterface

--- app/Interfaces/Services/LanguageServiceInterface.php
+++ app/Interfaces/Services/LanguageServiceInterface.php
@@ -7,4 +7,5 @@ interface LanguageServiceInterface
     public function index();
     public function store($request);
     public function update($request, $id);
+    public function destroy($id);
 }
