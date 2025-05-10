b6d19cbf1e5a35227862623395281c74846e4180
Dayles1 <Sololeveling0025@gmail.com>
2025-05-10 20:06:48 +0500

    + like Service index

--- app/Http/Controllers/Api/V1/User/LikeController.php
+++ app/Http/Controllers/Api/V1/User/LikeController.php
@@ -2,16 +2,19 @@
 namespace App\Http\Controllers\Api\V1\User;
 
 use App\Http\Controllers\Controller;
+use App\Interfaces\Services\LikeServiceInterface;
 use App\Models\Like;
 use App\Models\Book;
+use App\Services\LikeService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Resources\LikeResource;
 class LikeController extends Controller
 {
+    public function __construct(protected LikeServiceInterface $likeService){}
     public function index()
-    {
-        $likes = Like::with('book')->where('user_id', Auth::id())->paginate(10);
+    {   
+        $likes=$this->likeService->index();
         return $this->responsePagination($likes, LikeResource::collection($likes), __('message.like.show_success'));
     }
 
--- app/Services/LikeService.php
+++ app/Services/LikeService.php
@@ -2,12 +2,14 @@
 
 namespace App\Services;
 
+use App\Models\Like;
 use App\Interfaces\Services\LikeServiceInterface;
 
 class LikeService extends BaseService implements LikeServiceInterface
 {
     public function index(){
-
+        $likes = Like::with('book')->where('user_id', Auth::id())->paginate(10);
+        return $likes;
     }
     public function LikeDislike(){
 
