688e809ffa9423998d5cdb8879f38f45db394ae3
Dayles1 <Sololeveling0025@gmail.com>
2025-05-10 13:05:05 +0500

    php artisan make:provider InterfaceProvider

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,5 +3,6 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\ControllerServiceProvider::class,
+    App\Providers\InterfaceProvider::class,
     App\Providers\TelescopeServiceProvider::class,
 ];
