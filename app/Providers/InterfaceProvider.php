5d222a1744b39b68ce72c2d4ce7ee340c352239d
Dayles1 <Sololeveling0025@gmail.com>
2025-05-10 19:10:06 +0500

    bind to translations service

--- app/Providers/InterfaceProvider.php
+++ app/Providers/InterfaceProvider.php
@@ -7,6 +7,7 @@ use App\Services\BookService;
 use App\Services\UserService;
 use App\Services\CategoryService;
 use App\Services\LanguageService;
+use App\Services\TranslationService;
 use App\Services\ExchangeRateService;
 use Illuminate\Support\ServiceProvider;
 use App\Interfaces\Services\AuthServiceInterface;
@@ -14,6 +15,7 @@ use App\Interfaces\Services\BookServiceInterface;
 use App\Interfaces\Services\UserServiceInterface;
 use App\Interfaces\Services\CategoryServiceInterface;
 use App\Interfaces\Services\LanguageServiceInterface;
+use App\Interfaces\Services\TranslationServiceInterface;
 use App\Interfaces\Services\ExchangeRateServiceInterface;
 
 class InterfaceProvider extends ServiceProvider
@@ -29,6 +31,7 @@ class InterfaceProvider extends ServiceProvider
         $this->app->bind(UserServiceInterface::class, UserService::class);
         $this->app->bind(ExchangeRateServiceInterface::class, ExchangeRateService::class);
         $this->app->bind(LanguageServiceInterface::class, LanguageService::class);
+        $this->app->bind(TranslationServiceInterface::class, TranslationService::class);
     }
 
     /**
