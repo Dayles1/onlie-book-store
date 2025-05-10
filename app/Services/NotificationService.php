d5d32ef8aa4f66d9068e20a72622e0873982a826
Dayles1 <Sololeveling0025@gmail.com>
2025-05-10 20:20:28 +0500

    + methods to Notification service

--- app/Services/NotificationService.php
+++ app/Services/NotificationService.php
@@ -6,5 +6,18 @@ use App\Interfaces\Services\NotificationServiceInterface;
 
 class NotificationService extends BaseService implements NotificationServiceInterface
 {
-   
+    public function index()
+    {
+
+    }
+    public function show($id)
+    {
+
+    }
+    public function readed( $request){
+
+    }
+    public function unread( $request){
+        
+    }  
 }
