fcfa06cc196e1bf20a56d5f70fb721ced8999e95
Dayles1 <Sololeveling0025@gmail.com>
2025-05-10 20:18:27 +0500

    + methods to Notification interface

--- app/Interfaces/Services/NotificationServiceInterface.php
+++ app/Interfaces/Services/NotificationServiceInterface.php
@@ -4,5 +4,8 @@ namespace App\Interfaces\Services;
 
 interface NotificationServiceInterface
 {
-    //
+    public function index();
+    public function show($id);
+    public function readed( $request);
+    public function unread( $request);
 }
