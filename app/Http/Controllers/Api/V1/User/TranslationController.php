2e4e9928a5ce857742f059530f0ad76854826874
Dayles1 <Sololeveling0025@gmail.com>
2025-05-10 19:05:55 +0500

    +__constructor

--- app/Http/Controllers/Api/V1/User/TranslationController.php
+++ app/Http/Controllers/Api/V1/User/TranslationController.php
@@ -1,28 +1,24 @@
 <?php namespace App\Http\Controllers\Api\V1\User;
 
+use App\Interfaces\Services\TranslationServiceInterface;
 use App\Models\Translation;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\Cache;
+
 use App\Http\Requests\TranslationStoreRequest;
 use App\Http\Requests\TranslationUpdateRequest;
 
 class TranslationController extends Controller
 {
+    public function __construct(protected TranslationServiceInterface $translationService)
+    {
+    }
     public function index(Request $request)
     {
-        $locale = $request->header('locale');
-        
-        $translations = Cache::remember("translations_{$locale}", 3600, function () use ($locale) {
-            $translations = Translation::where('is_active', true);
-            if ($locale) {
-                $translations->where('locale', $locale);
-            }
-            return $translations->get();
-        });
-
+            $translations=$this->translationService->index($request);
         return $this->success($translations, __('message.translation.show_success'));
     }
+
 
    
 }
--- app/Services/TranslationService.php
+++ app/Services/TranslationService.php
@@ -2,13 +2,38 @@
 
 namespace App\Services;
 
-class TranslationService
+use App\Models\Translation;
+use Illuminate\Support\Facades\Cache;
+use App\Interfaces\Services\TranslationServiceInterface;
+
+class TranslationService extends BaseService implements TranslationServiceInterface
 {
-    /**
-     * Create a new class instance.
-     */
-    public function __construct()
+  public function index($request)
+  {
+     $locale = $request->header('locale');
+        
+        $translations = Cache::remember("translations_{$locale}", 3600, function () use ($locale) {
+            $translations = Translation::where('is_active', true);
+            if ($locale) {
+                $translations->where('locale', $locale);
+            }
+            return $translations->get();
+        });
+        return $translations;
+  }
+    public function store($request)
     {
-        //
+     
     }
+    public function update($request, $id)
+    {
+     
+       
+    }
+    public function destroy($id){
+
+    }
+   
+    
+
 }
