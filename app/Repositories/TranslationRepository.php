8ac214083060cd4ea493842df9bd93b492fb121f
Dayles1 <Sololeveling0025@gmail.com>
2025-05-13 18:09:19 +0500

    php artisan make:class Repositories/TranslationRepository

---
+++ app/Repositories/TranslationRepository.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Repositories;
+
+class TranslationRepository
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
