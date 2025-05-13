e05594278245b84d1a6cc9df32dd6114cfd3727a
Dayles1 <Sololeveling0025@gmail.com>
2025-05-13 18:06:29 +0500

    php artisan make:class Repositories/LikeRepository

---
+++ app/Repositories/LikeRepository.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Repositories;
+
+class LikeRepository
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
