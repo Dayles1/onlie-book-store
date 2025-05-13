b2f7471fc80aa8a2d79177404632ea43d68a4878
Dayles1 <Sololeveling0025@gmail.com>
2025-05-13 18:08:43 +0500

    php artisan make:class Repositories/LanguageRepository

---
+++ app/Repositories/LanguageRepository.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Repositories;
+
+class LanguageRepository
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
