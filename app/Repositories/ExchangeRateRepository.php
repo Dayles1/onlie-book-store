69e082355eef0636ea96c3faccbd4dbcd3871beb
Dayles1 <Sololeveling0025@gmail.com>
2025-05-13 18:10:52 +0500

    php artisan make:class Repositories/ExchangeRateRepository

---
+++ app/Repositories/ExchangeRateRepository.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Repositories;
+
+class ExchangeRateRepository
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
