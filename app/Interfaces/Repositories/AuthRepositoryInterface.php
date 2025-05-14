df5d4347c9dba6c5da3aa8fcb665fc137bf72e9d
Dayles1 <Sololeveling0025@gmail.com>
2025-05-14 14:22:41 +0500

    +interface methods to Auth

--- app/Interfaces/Repositories/AuthRepositoryInterface.php
+++ app/Interfaces/Repositories/AuthRepositoryInterface.php
@@ -4,5 +4,11 @@ namespace App\Interfaces\Repositories;
 
 interface AuthRepositoryInterface
 {
-    //
+    public function getAll();
+    public function find($find);
+    public function store($request);
+    public function update($request);
+    public function destroy();
+
+
 }
