aee5e21e584f0862cf6f9d78b62bd6c53175f56d
Dayles1 <Sololeveling0025@gmail.com>
2025-05-14 14:25:02 +0500

    +interface methods to User

--- app/Interfaces/Repositories/UserRepositoryInterface.php
+++ app/Interfaces/Repositories/UserRepositoryInterface.php
@@ -4,5 +4,9 @@ namespace App\Interfaces\Repositories;
 
 interface UserRepositoryInterface
 {
-    //
+    public function getAll();
+    public function find($find);
+    public function store($request);
+    public function update($request);
+    public function destroy();
 }
