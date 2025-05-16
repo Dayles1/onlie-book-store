39c4a1c41f9ba344bffb08382e5a8a90eb5c3012
Dayles1 <Sololeveling0025@gmail.com>
2025-05-16 23:39:05 +0500

    +uploadPhoto and deletePhoto to BaseRepository

--- app/Repositories/BaseRepository.php
+++ app/Repositories/BaseRepository.php
@@ -27,4 +27,16 @@ class BaseRepository
 
         return $preparedTranslations;
     }
+       public function uploadPhoto($file, $path='uploads')
+    {
+       $photoName = md5(time().$file->getFilename()).'.'.$file->getClientOriginalExtension();
+       return $file->storeAs($path, $photoName, 'public');
+    }
+    public function deletePhoto($path)
+    {
+        $fullPath = storage_path('app/public/' . $path);
+        if (file_exists($fullPath)) {
+            unlink($fullPath);
+        }
+    }
      }
