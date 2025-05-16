db54c9865fac437c1b2503944ba0b11311bcc3e8
Dayles1 <Sololeveling0025@gmail.com>
2025-05-16 18:43:34 +0500

    php  artisan make:class Respositories/BaseRepository

--- app/Repositories/CategoryRepository.php
+++ app/Repositories/CategoryRepository.php
@@ -11,7 +11,17 @@ class CategoryRepository implements CategoryRepositoryInterface
            return Category::paginate(10);
     }
     public function find($find){}
-    public function store($request){}
+    public function store($data){
+        $category = new Category([
+            'parent_id' => $data['parent_id'] ?? null,
+        ]);
+        
+
+        $translations = $this->prepareTranslations($data['translations'], ['title']);
+
+        $category->fill($translations)->save();
+        return $category;
+    }
     public function update($request){}
     public function destroy(){}
     
--- app/Services/CategoryService.php
+++ app/Services/CategoryService.php
@@ -29,14 +29,7 @@ class CategoryService extends BaseService implements CategoryServiceInterface
     }
     public function store($data)
     {   
-        $category = new Category([
-            'parent_id' => $data['parent_id'] ?? null,
-        ]);
-        
-
-        $translations = $this->prepareTranslations($data['translations'], ['title']);
-
-        $category->fill($translations)->save();
+        $category=$this->categoryRepository->store($data);
         return $category;
     
     }
