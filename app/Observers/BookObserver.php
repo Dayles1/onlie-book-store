240c0a2bca1eea00451ae43fd0a9acc801d34905
Dayles1 <Sololeveling0025@gmail.com>
2025-05-20 22:31:45 +0500

    upgrade book slug generator and - created meyhod

--- app/Observers/BookObserver.php
+++ app/Observers/BookObserver.php
@@ -6,29 +6,28 @@ use Illuminate\Support\Str;
 
 class BookObserver
 {
-    private function generateUniqueSlug($title)
-    {
-        $baseSlug = Str::slug($title);
-        $slug = $baseSlug;
-        $count = 1;
+   private function generateUniqueSlug($title)
+{
+    $baseSlug = Str::slug($title);
 
-        while (Book::where('slug', $slug)->exists()) {
-            $slug = $baseSlug . '-ID' . $count;
-            $count++;
-        }
-        return $slug;
+    $existingSlugs = Book::where('slug', 'LIKE', $baseSlug . '%')
+        ->pluck('slug')
+        ->toArray();
+
+    if (!in_array($baseSlug, $existingSlugs)) {
+        return $baseSlug;
     }
 
-    public function created(Book $book): void
-    {
-        $title = $book->translations->firstWhere('locale', 'en')->title;
-        $slug = $this->generateUniqueSlug($title);
-
-        Book::withoutEvents(function () use ($book, $slug) {
-            $book->slug = $slug;
-            $book->save();
-        });
+    $i = 1;
+    while (in_array($baseSlug . '-id' . $i, $existingSlugs)) {
+        $i++;
     }
+
+    return $baseSlug . '-id' . $i;
+}
+
+
+ 
 
     public function updated(Book $book): void
     {
