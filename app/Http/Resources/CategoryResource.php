25344107602aee7f6cff2e7ca5a7e3335d0fae36
Dayles1 <Sololeveling0025@gmail.com>
2025-05-20 15:18:39 +0500

    fix category_translations

--- app/Http/Resources/CategoryResource.php
+++ app/Http/Resources/CategoryResource.php
@@ -18,7 +18,9 @@ class CategoryResource extends JsonResource
             'children' => CategoryResource::collection($this->whenLoaded('children')),
             'books' => BookResource::collection($this->whenLoaded('books')),
             'parent' => new CategoryResource($this->whenLoaded('parent')),
+            // 'translations' => new CategoryTranslationResource($this->whenLoaded('translations')),
             'translations' => $this->whenLoaded('translations'),
+            
         ];
     }
 }
--- app/Http/Resources/CategoryTranslationResource.php
+++ app/Http/Resources/CategoryTranslationResource.php
@@ -15,8 +15,7 @@ class CategoryTranslationResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'locale' => $this->locale,
-            'title' => $this->title,
+            
         ];
     }
 }
