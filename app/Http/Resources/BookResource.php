7e2530d71d4693ff78c7d6d6bc7e5247755eecef
Dayles1 <Sololeveling0025@gmail.com>
2025-05-21 09:47:17 +0500

    +translations to BookResorce

--- app/Http/Resources/BookResource.php
+++ app/Http/Resources/BookResource.php
@@ -32,6 +32,7 @@ class BookResource extends JsonResource
             'likes' => $this->likes->count(),
             'price_uzs' => $priceInUzs . ' UZS',
             'prices' => $pricesInOtherCurrencies, 
+            'translations' => $this->whenLoaded('translations', fn () => $this->translations->pluck('title', 'locale')),
             'categories' => $this->whenLoaded('categories', fn () => $this->categories->pluck('title')),
             'images' => $this->whenLoaded('images', fn () => $this->images->pluck('url')),
             'created_at' => $this->created_at->toDateTimeString(),
