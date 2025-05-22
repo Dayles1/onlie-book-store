bd37a04b7edb20cc31e7bbbab9b747d12e87883f
Dayles1 <Sololeveling0025@gmail.com>
2025-05-22 19:29:02 +0500

    +AuthRepositoryInterface refreshToken method

--- app/Interfaces/Repositories/AuthRepositoryInterface.php
+++ app/Interfaces/Repositories/AuthRepositoryInterface.php
@@ -9,7 +9,8 @@ interface AuthRepositoryInterface
     public function find($find);
     public function store(AuthDTO $request);
     public function deleteToken($user);
-     public function createToken($user);
+    public function createToken($user);
+    public function refreshToken($user);
 
 
 }
