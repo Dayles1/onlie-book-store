abd765d6d22acf3101076d453c79623f3ae4ef12
Dayles1 <Sololeveling0025@gmail.com>
2025-05-23 16:01:55 +0500

    hello

--- app/DTO/CategoryDTO.php
+++ app/DTO/CategoryDTO.php
@@ -4,11 +4,20 @@ namespace App\DTO;
 
 class CategoryDTO
 {
-    /**
-     * Create a new class instance.
-     */
-    public function __construct()
+    public array $translations;
+    public ?int $parent_id;
+
+    public function __construct(array $translations, ?int $parent_id = null)
     {
-        //
+        $this->translations = $translations;
+        $this->parent_id = $parent_id;
+    }
+
+    public static function fromArray(array $data): self
+    {
+        return new self(
+            translations: $data['translations'] ?? [],
+            parent_id: isset($data['parent_id']) ? (int) $data['parent_id'] : null,
+        );
     }
 }
