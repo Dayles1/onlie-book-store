6183c0ac090109a4ab3e66ebe104f87ffdd0a261
Dayles1 <Sololeveling0025@gmail.com>
2025-05-23 17:19:46 +0500

    +ExchangeRateDTO logic

--- app/DTO/ExchangeRateDTO.php
+++ app/DTO/ExchangeRateDTO.php
@@ -2,13 +2,26 @@
 
 namespace App\DTO;
 
+use Carbon\Carbon;
+
 class ExchangeRateDTO
 {
-    /**
-     * Create a new class instance.
-     */
-    public function __construct()
+    public string $code;
+    public float $rate;
+    public Carbon $date;
+
+    public function __construct(string $code, float $rate, string $date)
     {
-        //
+        $this->code = $code;
+        $this->rate = $rate;
+        $this->date = Carbon::parse($date);
+    }
+    public static function fromArray(array $data): self
+    {
+        return new self(
+            code: $data['code'] ?? '',
+            rate: isset($data['rate']) ?? null,
+            date: $data['date'] ?? now()->toDateString(),
+        );
     }
 }
